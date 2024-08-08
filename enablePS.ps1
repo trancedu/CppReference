8a863b76c6123c8ff3952362d8b034da22ff702c
trancedu <92005446+trancedu@users.noreply.github.com>
2024-08-08 17:25:03 -0400

    Use separate EnablePSRemoting.ps1

--- enablePS.ps1
+++ enablePS.ps1
@@ -1,18 +1,38 @@
-$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -Command `"Enable-PSRemoting -Force; gpupdate /force`""
-$trigger = New-ScheduledTaskTrigger -Daily -At 3am
-$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+# C:\service\EnablePSRemoting.ps1
 
-Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -Settings $settings -TaskName "EnableWinRMTask" -Description "Enable WinRM and update group policy"
+# Define the log file path
+$logFile = "C:\service\EnablePSRemoting.log"
 
-# Define the PowerShell command to run
-$command = 'Enable-PSRemoting -Force'
+# Write start time to the log file
+"Start: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')" | Out-File -FilePath $logFile -Append
 
-# Create the scheduled task action
-$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -Command `"$command`""
+# Enable PSRemoting
+Enable-PSRemoting -Force
 
-# Define the PowerShell command to run
-$psCommand = 'Enable-PSRemoting -Force'
+# Write operation message to the log file
+"PSRemoting Enabled" | Out-File -FilePath $logFile -Append
+
+# Write end time to the log file
+"End: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')" | Out-File -FilePath $logFile -Append
+
+
+# Define the path to the PowerShell script
+$scriptPath = "C:\service\EnablePSRemoting.ps1"
 
 # Create the scheduled task action using cmd.exe to invoke PowerShell
-$action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c powershell.exe -NoProfile -ExecutionPolicy Bypass -Command `"$psCommand`""
+$action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c powershell.exe -NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`""
+
+# Create the trigger to run once, one minute from now
+$trigger = New-ScheduledTaskTrigger -Once -At ((Get-Date).AddMinutes(1))
+
+# Create the principal to run as SYSTEM with the highest privileges
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+# Create the settings
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+
+# Register the scheduled task
+Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -Settings $settings -TaskName "EnableWinRMTask" -Description "Enable WinRM"
+
+# Output the task for verification
+Get-ScheduledTask -TaskName "EnableWinRMTask"
